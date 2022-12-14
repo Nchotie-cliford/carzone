7c5235d0b6cfc70110e649fca4c69b15e82168f3
nchotie ndonwie <wixsauber@gmail.com>
2022-12-14 14:41:42 +0100

    staic files, html/css seprations

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,15 @@
 
 from django.urls import path
 from .views import home
+from .views import about
+from .views import services
+from .views import contact
+
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('Services', services, name='services'),
+    path('Contact', contact, name='contact'),
+
 ]
